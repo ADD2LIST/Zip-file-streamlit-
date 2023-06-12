f1aa728712f72d31933831d49aee4ccd826821c0
Bibek Pradhan <134042662+ADD2LIST@users.noreply.github.com>
2023-06-12 09:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,55 +2,53 @@ import streamlit as st
 
 import zipfile
 
-import tempfile
+def convert_file_to_zip(file_path):
 
-import os
+  """Converts a file to a zip file.
 
-def convert_to_zip(file_path):
+  Args:
 
-    directory, filename = os.path.split(file_path)
+    file_path: The path to the file to convert.
 
-    zip_path = os.path.join(directory, f"{filename}.zip")  # Set the output zip file path
+  Returns:
+
+    A zip file object.
+
+  """
+
+  with zipfile.ZipFile(file_path + ".zip", "w") as zip_file:
+
+    zip_file.write(file_path)
+
+def main():
+
+  """The main function of the Streamlit application."""
+
+  # Get the file to convert.
+
+  file_path = st.file_uploader("Upload a file", type=["*"])
+
+  # If a file was uploaded, convert it to a zip file and provide a download option.
+
+  if file_path is not None:
+
+    with open(file_path, "rb") as f:
+
+      zip_file = convert_file_to_zip(f.name)
+
+    st.download_button(
+
+      label="Download Zip File",
+
+      data=zip_file,
+
+      file_name=file_path.name + ".zip"
+
+    )
+
+if __name__ == "__main__":
+
+  main()
+
 
     
-
-    with zipfile.ZipFile(zip_path, 'w') as zip_file:
-
-        zip_file.write(file_path, arcname=filename)  # Add the file to the zip archive
-
-    
-
-    return zip_path
-
-# Streamlit app
-
-st.title("File to Zip Converter")
-
-file_to_convert = st.file_uploader("Choose a file to convert", type=["txt", "csv", "pdf"])  # Allow only specific file types
-
-if file_to_convert is not None:
-
-    st.write("Converting...")
-
-    
-
-    # Save the uploaded file to a temporary directory on disk
-
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-
-        temp_file.write(file_to_convert.read())
-
-        temp_file_path = temp_file.name
-
-    
-
-    converted_file = convert_to_zip(temp_file_path)
-
-    st.write(f"File '{file_to_convert.name}' successfully converted to '{converted_file}'.")
-
-    
-
-    st.download_button("Download Zip File", converted_file, file_to_convert.name + ".zip", key='download_button')
-
-
-
